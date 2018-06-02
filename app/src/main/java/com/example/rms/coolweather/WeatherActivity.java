55440baf5e31c4e7f3b024ed52a3d4864660877e
rms <rms_coder@163.com>
2018-06-02 18:19:34 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/rms/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/rms/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.rms.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.rms.coolweather.gson.Forecast;
 import com.example.rms.coolweather.gson.Weather;
+import com.example.rms.coolweather.service.AutoUpdateService;
 import com.example.rms.coolweather.util.HttpUtil;
 import com.example.rms.coolweather.util.Utility;
 
@@ -151,6 +153,7 @@ public class WeatherActivity extends AppCompatActivity {
             public void onResponse(Call call, Response response) throws IOException {
                 final  String responseText = response.body().string();
                 final  Weather weather = Utility.handleWeatherResponse(responseText);
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -159,6 +162,7 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+
                         }else{
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
@@ -227,6 +231,8 @@ public class WeatherActivity extends AppCompatActivity {
             sportText.setText(sport);
             weatherLayout.setVisibility(View.VISIBLE);
         }
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startService(intent);
     }
 
 }
